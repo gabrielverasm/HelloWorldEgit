558894d578de46888f3670afea725e47dcf39267
Gabriel Veras <gabrielverasm@gmail.com>
2017-07-02 22:04:37 -0300

    Identando código..

--- src/ImprimirConsole.java
+++ src/ImprimirConsole.java
@@ -2,8 +2,7 @@
 public class ImprimirConsole {
 
 	public static void main(String[] args) {
-System.out.println("Hello Git, Egit and GitHub!!");
-		
+		System.out.println("Hello Git, Egit and GitHub!!");
 		System.out.println("Hello in github");
 	}
 
