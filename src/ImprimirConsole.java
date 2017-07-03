40e194bf61020afcb74a29919cb91c183813ac2b
Gabriel Veras <gabrielverasm@gmail.com>
2017-07-02 22:07:53 -0300

    Correção de teste

--- src/ImprimirConsole.java
+++ src/ImprimirConsole.java
@@ -4,7 +4,6 @@ public class ImprimirConsole {
 	public static void main(String[] args) {
 		System.out.println("Hello Git, Egit and GitHub!!");
 		System.out.println("Hello in github");
-		System.out.println("More tests");
 	}
 
 }
