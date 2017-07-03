84a4a512f73146e51b2dee59b2d56f0baad8029f
Gabriel Veras <gabrielverasm@gmail.com>
2017-07-02 21:58:10 -0300

    Alterando a impressão no github

--- src/ImprimirConsole.java
+++ src/ImprimirConsole.java
@@ -3,6 +3,8 @@ public class ImprimirConsole {
 
 	public static void main(String[] args) {
 System.out.println("Hello Git, Egit and GitHub!!");
+		
+		System.out.println("Hello in github");
 	}
 
 }
