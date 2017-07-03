ac555114604c01aaadaa0e72aed85f0794211f90
Gabriel Veras <gabrielverasm@gmail.com>
2017-07-02 22:06:44 -0300

    Mais testes

--- src/ImprimirConsole.java
+++ src/ImprimirConsole.java
@@ -4,6 +4,7 @@ public class ImprimirConsole {
 	public static void main(String[] args) {
 		System.out.println("Hello Git, Egit and GitHub!!");
 		System.out.println("Hello in github");
+		System.out.println("More tests");
 	}
 
 }
